47c6a4bd1d55113591c8e4cb721ba07053c50397
Abhishek Singh <abhishekrathorerispurpose@gmail.com>
2024-11-19 11:23:28 +0530

    "Checks"

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from validation_model import Post
+
 
 app = FastAPI()
-
-# code of get decorator
-
 
 @app.get("/")
 async def root():
@@ -17,12 +16,7 @@ async def root():
         return {"error": str(e)}
 
 @app.post("/post")
-async def post(payload:dict=Body(...)):
-    try:
-        if payload is None:
-            raise ValueError("Payload is None")
-        if 'name' not in payload:
-            raise ValueError("Payload does not contain 'name'")
-        return {"message": f"Hello World {payload['name']}"} 
-    except Exception as e:
-        return {"error": str(e)}
+async def post(post:Post):
+    return post.model_dump()
+
+    
