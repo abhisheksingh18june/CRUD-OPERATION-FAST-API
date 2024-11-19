b8ff9434bc1bc28713b586d3b81b6bb95b841d5e
Abhishek Singh <abhishekrathorerispurpose@gmail.com>
2024-11-19 17:37:00 +0530

    "Validation check and error returning"

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI ,Request,Response,status
 from fastapi.params import Body
 from validation_model import Post
-
+import sys
+import random
 
 app = FastAPI()
 
+my_post=[{
+    "title": "title one",
+    "content": "content one",
+    "id": 1},
+    {
+    "title": "title two",
+    "content": "content two",
+    "id": 2},
+    {
+    "title": "title three",
+    "content": "content three",
+    "id": 3}]
+
+def fetch_post(id):
+    for post in my_post:
+        if post["id"]==id:
+            return post
+        
 @app.get("/")
 async def root():
     try:
@@ -15,8 +34,30 @@ async def root():
     except Exception as e:
         return {"error": str(e)}
 
-@app.post("/post")
+@app.post("/posts")
 async def post(post:Post):
-    return post.model_dump()
+    post_dict=post.model_dump()
+    post_dict['id']=random.randint(1,1000)
+    my_post.append(post_dict)
+    return {"data": post_dict}
+
+@app.get("/posts/latest")
+async def get_latest_post():
+    post= my_post[-1]
+    return {"post":post}
+
+
+@app.get("/posts/{id}")
+async def get_post(id:int,response:Response):
+    post= fetch_post(id)
+    if post is None:
+        response.status_code=status.HTTP_404_NOT_FOUND
+        return {"error":"post not found"}
+    return {"post":post}
+
+@app.get("/posts")
+async def get_posts():
+    return {"data": my_post}
+
 
     
--- validation_model.py
+++ validation_model.py
@@ -3,10 +3,13 @@ from typing import Optional
 
 class Post(BaseModel):
     '''
-    This is a pydantic model for the post class which checks whether the name is a string, age is an integer, published is a boolean and rating is optional. 
+    This is a pydantic model for the post class which checks whether the title is a string, content is a string, published is a boolean ,rating is optional and id is an integer. 
     '''
-    name: str
-    age: int
+    title: str
+    content: str
     published: bool = True
     rating: Optional[int] = None
+    id: Optional[int] = None
 
+    class Config:
+        extra = "forbid"
